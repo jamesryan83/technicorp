d965249fc9326f0efeff840207f0fc8957cc0f60
James <james4165@hotmail.com>
2016-05-30 07:14:55 +1000

    preload images

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,6 +2,24 @@
 
 window.onload = function () {
 
+    // load svgs and images that arean't loaded on startup
+    var urls = [
+        "/res/svg/icon_background_dialog.svg",
+        "/res/svg/icon_close_hover.svg",
+        "/res/svg/icon_handle_hover.svg",
+        "/res/svg/icon_nav_area.svg",
+        "/res/images/p/p1b.jpg",
+        "/res/images/p/p2b.jpg",
+        "/res/images/p/p3b.jpg",
+        "/res/images/p/p4b.jpg"
+    ]
+
+    for (var i = 0; i < urls.length; i++) {
+        var img = new Image();
+        img.src = urls[i];
+    }
+
+
     // setup subheading animated text
     $("#subheading").airport([ 'moscow', 'berlin', 'stockholm', 'new york', 'tokyo', 'sydney', 'london' ]);
 
